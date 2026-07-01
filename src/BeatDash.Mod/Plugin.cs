161104394aee2781a1084eddec19aae64cb63e81
Shiron <contact@shiron.io>
2026-07-01 12:12:18 +0200

    feat(mod): Added settings menu

--- src/BeatDash.Mod/Plugin.cs
+++ src/BeatDash.Mod/Plugin.cs
@@ -1,5 +1,8 @@
+using BeatSaberMarkupLanguage.Settings;
+using BeatSaberMarkupLanguage.Util;
 using IPA;
 using IPA.Loader;
+using Shiron.BeatDash.Mod.UI;
 using IpaLogger = IPA.Logging.Logger;
 
 namespace Shiron.BeatDash.Mod;
@@ -7,6 +10,8 @@ namespace Shiron.BeatDash.Mod;
 [Plugin(RuntimeOptions.DynamicInit)]
 internal class Plugin {
     internal static IpaLogger Log { get; private set; } = null!;
+
+    private readonly Settings _settings = new();
 
     [Init]
     public Plugin(IpaLogger ipaLogger, PluginMetadata pluginMetadata) {
@@ -17,6 +22,16 @@ internal class Plugin {
     [OnStart]
     public void OnApplicationStart() {
         Log.Debug("OnApplicationStart");
+
+        MainMenuAwaiter.MainMenuInitializing += MainMenuInit;
+    }
+
+    private void MainMenuInit() {
+        BSMLSettings.Instance.AddSettingsMenu(
+            "BeatDash",
+            "Shiron.BeatDash.Mod.UI.Settings.bsml",
+            _settings
+        );
     }
 
     [OnExit]
---
+++ src/BeatDash.Mod/UI/Settings.cs
@@ -0,0 +1,8 @@
+using BeatSaberMarkupLanguage.Util;
+
+namespace Shiron.BeatDash.Mod.UI;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+public class Settings : PersistentSingleton<Settings> {
+#pragma warning restore CS0618 // Type or member is obsolete
+}
